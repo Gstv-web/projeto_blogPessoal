547fad01a5b62d098e2047cbd439edf013289bc5
José Gustavo Oliveira da Silva <gustavo.myw@live.com>
2022-02-16 15:55:16 -0300

    Spring server initializer

---
+++ src/main/java/org/generation/blogPessoal/ServerInitializer.java
@@ -0,0 +1,12 @@
+package org.generation.blogPessoal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServerInitializer extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BlogPessoalApplication.class);
+    }
+}
